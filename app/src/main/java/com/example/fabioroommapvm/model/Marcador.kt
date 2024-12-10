5f423bff7beb8073e43adabbcb1c99bd7b5e0f38
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-10 18:34:04 +0100

    Creamos el dataclass Marcador

---
+++ app/src/main/java/com/example/fabioroommapvm/model/Marcador.kt
@@ -0,0 +1,9 @@
+package com.example.fabioroommapvm.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Marcadores")
+data class Marcador(
+)
