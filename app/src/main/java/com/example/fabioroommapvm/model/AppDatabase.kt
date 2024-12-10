20095424ed6793551b9f973f1b2d753d2a2d0b6f
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-10 18:45:02 +0100

    Creamos la función para obtener la base de datos

--- app/src/main/java/com/example/fabioroommapvm/model/AppDatabase.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/AppDatabase.kt
@@ -11,6 +11,27 @@ import kotlinx.coroutines.launch
 
 @Database(entities = [Marcador::class, TipoMarcador::class], version = 8)
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun marcadorDao(): MarcadorDao
+    abstract fun tipoMarcadorDao(): TipoMarcadorDao
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+        @OptIn(DelicateCoroutinesApi::class)
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "marcadores_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                GlobalScope.launch {
+                    datosIniciales(instance.tipoMarcadorDao(), instance.marcadorDao())
+                }
+                instance
+            }
+        }
 
-
+    }
 }
