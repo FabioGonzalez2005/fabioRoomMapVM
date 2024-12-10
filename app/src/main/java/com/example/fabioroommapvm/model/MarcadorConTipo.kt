ef748c871c45c4f5ae54826ca1f083137ed2eb8b
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-10 18:43:36 +0100

    Realizamos la foreign key

--- app/src/main/java/com/example/fabioroommapvm/model/MarcadorConTipo.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/MarcadorConTipo.kt
@@ -4,5 +4,10 @@ import androidx.room.Embedded
 import androidx.room.Relation
 
 data class MarcadorConTipo(
-
+    @Embedded val marcador: Marcador,
+    @Relation(
+        parentColumn = "idTipoMarcadorOwner",
+        entityColumn = "idTipoMarcador"
+    )
+    val tiposMarcadores: List<TipoMarcador>
 )
