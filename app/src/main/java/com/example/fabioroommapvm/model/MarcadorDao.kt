eb339e365fe347a3a3cc124a8196e5b8de77e039
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-10 18:31:44 +0100

    Creamos MarcadorDao.kt

---
+++ app/src/main/java/com/example/fabioroommapvm/model/MarcadorDao.kt
@@ -0,0 +1,12 @@
+package com.example.fabioroommapvm.model
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MarcadorDao {
+
+
+}
