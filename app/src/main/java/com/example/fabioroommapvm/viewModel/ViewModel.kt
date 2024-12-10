869808e069755ae98867593b6423ec653c8167ab
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-10 18:30:07 +0100

    Creamos la clase ViewModelFactory

--- app/src/main/java/com/example/fabioroommapvm/viewModel/ViewModel.kt
+++ app/src/main/java/com/example/fabioroommapvm/viewModel/ViewModel.kt
@@ -3,4 +3,12 @@ package com.example.fabioroommapvm.viewModel
 class MarcadorVistaModelo(private val marcadorDao: MarcadorDao) : ViewModel() {
     val marcadoresConTipo: Flow<List<MarcadorConTipo>> = marcadorDao.obtenerTodosMarcadoresYTipos()
 }
+
+class ViewModelFactory(private val marcadorDao: MarcadorDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MarcadorVistaModelo::class.java)) {
+            return MarcadorVistaModelo(marcadorDao) as T
+        }
+        throw IllegalArgumentException("Clase ViewModel desconocida")
+    }
 }
