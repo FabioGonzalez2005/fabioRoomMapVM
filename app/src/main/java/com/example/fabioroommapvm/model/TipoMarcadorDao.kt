06686f3c6c0c13d8b0b53f493af8dafb5b9a9dc2
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-10 18:33:26 +0100

    Añadimos la función obtenerTodosTipos

--- app/src/main/java/com/example/fabioroommapvm/model/TipoMarcadorDao.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/TipoMarcadorDao.kt
@@ -10,4 +10,7 @@ interface TipoMarcadorDao {
 
     @Insert
     suspend fun insertarTipoMarcador(tipoMarcador: TipoMarcador)
+
+    @Query("SELECT * FROM TiposMarcadores")
+    fun obtenerTodosTipos(): Flow<List<TipoMarcador>>
 }
