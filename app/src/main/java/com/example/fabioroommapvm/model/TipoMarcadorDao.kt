95f13368f310708ef5cbe209d47c11ac5b595fab
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-10 18:32:53 +0100

    Añadimos TipoMarcadorDao

---
+++ app/src/main/java/com/example/fabioroommapvm/model/TipoMarcadorDao.kt
@@ -0,0 +1,10 @@
+package com.example.fabioroommapvm.model
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TipoMarcadorDao {
+}
